5f861d67a7788aeaa08d7019513ba86763be4c9b
craigwann <craigwann@me.com>
2022-02-28 15:48:28 -0800

    JS variable scope practice

---
+++ js/vscopeScripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+  $("div#click-one").click(function() {
+    const whatToSay = "Hello!";
+    alert(whatToSay);
+  });
+
+  $("div#click-two").click(function() {
+    alert(whatToSay);
+  });
+
+});
